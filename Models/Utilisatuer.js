f2b3106747ef30d3c36bc85d1f70268862e080e3
oumaimaakaichi <kthiri.houssem.eddine>
2024-03-19 13:27:58 +0100

    first

---
+++ Models/Utilisatuer.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    nom : {
+        type : String,
+        required: true
+    },
+    prenom:{
+        type:String
+    },
+   
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String
+    },
+    Num_tel: {
+        type:Number,
+        maxLength:8
+    },
+   age:{
+    type:Number,
+
+   },
+   nbr_enfants:
+   {
+    type:Number
+   }
+   
+   
+})
+
+const Utilisateur = mongoose.model('Utilisateur', schema);
+
+module.exports = Utilisateur;
